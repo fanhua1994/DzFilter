80be92a304d5db5166d7b4895fd9b48e36b6a636
fanhua1994 <18383219470@163.com>
2017-12-22 11:39:32 +0800

    更新代码

---
+++ src/main/java/com/zhiweism/controller/App3.java
@@ -0,0 +1,12 @@
+package com.zhiweism.controller;
+
+import com.zhiweism.text.utils.TextUtils;
+
+public class App3 {
+	
+	public static void main(String[] args) {
+		String s = TextUtils.filter("你好董志平,欢迎您");
+		System.out.println(s);
+	}
+
+}
